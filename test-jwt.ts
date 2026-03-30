7fa9e0ac8fe03c2d815968b514d2c91c9cf259d4
ankit-pfc <ankitmishra@projectfitco.com>
2026-03-30 09:58:57 +0530

    chore: remove temporary test file

--- test-jwt.ts
+++
@@ -1,6 +0,0 @@
-import { google } from 'googleapis';
-const jwtClient = new google.auth.JWT({
-  email: 'test@example.com',
-  key: 'private_key',
-  scopes: ['https://www.googleapis.com/auth/indexing'],
-});
